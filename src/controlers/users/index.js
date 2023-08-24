efcf0df4ea96f1f175167ffcb205f3dc54508071
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-08-24 17:56:09 +0300

    buggs current1

--- src/controlers/users/index.js
+++ src/controlers/users/index.js
@@ -3,7 +3,7 @@ const { verifyEmail } = require("./verifyEmail");
 const { resendVerifyEmail } = require("./resendVerifyEmail");
 const { logIn } = require("./logIn");
 const { logOut } = require("./logOut");
-const getCurrent = require("./getCurrent");
+const { getCurrent } = require("./getCurrent");
 module.exports = {
   register,
   verifyEmail,
