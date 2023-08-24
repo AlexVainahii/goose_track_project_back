83a2f838c0651c9f1bb76c88c2b1d28dfcb510d2
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-08-24 13:21:29 +0300

    reg

--- src/routes/users/users.js
+++ src/routes/users/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.post("/register", (req, res, next) => {
   res.status(200).json({
-    user: 1,
+    message: "register",
   });
 });
 router.get("/", (req, res, next) => {
