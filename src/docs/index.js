410058cae6098f8c8d4a5e28ee1d1e7165411072
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-08-26 00:44:41 +0300

    docs2

--- src/docs/index.js
+++ src/docs/index.js
@@ -1,13 +1,13 @@
 const basicInfo = require("./basicInfo");
 const servers = require("./servers");
-// const components = require("./components");
+const components = require("./components");
 const tags = require("./tags");
-// const paths = require("./paths");
+const paths = require("./paths");
 
 module.exports = {
   ...basicInfo,
   ...servers,
-  //   ...components,
+  ...components,
   ...tags,
-  //   ...paths,
+  ...paths,
 };
