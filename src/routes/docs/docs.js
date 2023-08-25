3508e47f093f8e065c9aec9f4cc4d23fa8979c6c
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-08-26 01:13:49 +0300

    fix bugs docs2

--- src/routes/docs/docs.js
+++ src/routes/docs/docs.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("@src/docs");
+const swaggerDocument = require("../../docs");
 const router = express.Router();
 
 const options = {
