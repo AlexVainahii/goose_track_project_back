02041401c4a3f7ea80638ec0a3704d47b041b25d
Andrew Ketsko <monicman82@gmail.com>
2023-08-28 19:39:53 +0300

    docs done

--- src/docs/paths/tasks/index.js
+++ src/docs/paths/tasks/index.js
@@ -1,6 +1,16 @@
+// module.exports = {
+//   "/(get)": require("./getMonthTasks"),
+//   "/(post)": require("./postTask"),
+//   "/:id(patch)": require("./patchTask"),
+//   "/:id(delete)": require("./deleteTask"),
+// };
+
+const getMonthTasks = require("./getMonthTasks");
+const postTask = require("./postTask");
+const patchTask = require("./patchTask");
+const deleteTask = require("./deleteTask");
+
 module.exports = {
-  "/(get)": require("./getMonthTasks"),
-  "/(post)": require("./postTask"),
-  "/:id(patch)": require("./patchTask"),
-  "/:id(delete)": require("./deleteTask"),
+  "/tasks": { ...getMonthTasks, ...postTask},
+  "/tasks/:id": {...patchTask, ...deleteTask },
 };
