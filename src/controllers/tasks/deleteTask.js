02e080fe924e2df12d296fe6f226c2efe8632c77
Andrew Ketsko <monicman82@gmail.com>
2023-08-30 20:01:45 +0300

    done

--- src/controllers/tasks/deleteTask.js
+++ src/controllers/tasks/deleteTask.js
@@ -6,7 +6,7 @@ const deleteTask = async (req, res) => {
   const result = await Task.findByIdAndRemove(id);
   CheckByError(!result, 404);
   result.owner = undefined;
-  res.status(204).json({data:result, status:204});
+  res.json({data:result, status:200});
 };
 
 module.exports = { deleteTask };
