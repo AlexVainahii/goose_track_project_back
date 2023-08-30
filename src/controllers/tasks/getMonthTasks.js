4abe54e88124381dd207d12acbc34565fc7eade7
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-08-30 17:00:27 +0300

    tasks

--- src/controllers/tasks/getMonthTasks.js
+++ src/controllers/tasks/getMonthTasks.js
@@ -4,11 +4,11 @@ const getMonthTasks = async (req, res) => {
   const result = await Task.find(
     {
       owner: req.user._id,
-      date: { $regex: req.body.date, $options: "i" },
+      date: { $regex: req.query.date, $options: "i" },
     },
     "-owner"
   );
-  res.json({data:result, status:200});
+  res.json({ data: result, status: 200 });
 };
 
 module.exports = { getMonthTasks };
