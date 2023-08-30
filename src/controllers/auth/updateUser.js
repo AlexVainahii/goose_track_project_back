be216f55d04d05d9544ebec63404d00404b415af
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-08-30 19:20:58 +0300

    docs 3

--- src/controllers/auth/updateUser.js
+++ src/controllers/auth/updateUser.js
@@ -3,9 +3,11 @@ const asyncHandler = require("express-async-handler");
 
 const updateUser = async (req, res, next) => {
   const { _id } = req.user;
+
   const { email, password, userName, skype, phone, birthDay, avatarURL } =
     req.body;
   const updatedFields = {};
+
   if (email) updatedFields.email = email;
   if (password) updatedFields.password = password;
   if (userName) updatedFields.userName = userName;
@@ -13,17 +15,17 @@ const updateUser = async (req, res, next) => {
   if (phone) updatedFields.phone = phone;
   if (birthDay) updatedFields.birthDay = birthDay;
   if (avatarURL) updatedFields.avatarURL = avatarURL;
-  if (req.file || !avatarURL) {
+
+  if (req.file) {
     const avatarURL = req.file.path;
     updatedFields.avatarURL = avatarURL;
   }
+
   await UserService.updated(_id, updatedFields);
-  res
-    .status(200)
-    .json({
-      status: 200,
-      message: "User updated successfully",
-      user: updatedFields,
-    });
+  res.status(200).json({
+    status: 200,
+    message: "User updated successfully",
+    user: updatedFields,
+  });
 };
 module.exports = { updateUser: asyncHandler(updateUser) };
--- src/middlewares/dontBody.js
+++ src/middlewares/dontBody.js
@@ -1,11 +1,7 @@
 const helpers = require("@helpers");
 
 const dontBody = (req, res, next) => {
-  console.log("req.file)>> ", req.file);
-  if (
-    (Object.keys(req.body).length === 0) &
-    (Object.keys(req.file).length === 0)
-  ) {
+  if (!req.file & (Object.keys(req.body).length === 0)) {
     console.log("req :>> ", req);
     switch (req.method) {
       case "PATCH":
@@ -15,7 +11,6 @@ const dontBody = (req, res, next) => {
         throw helpers.CreateError(400, "missing fields");
     }
   }
-
   next();
 };
 module.exports = dontBody;
