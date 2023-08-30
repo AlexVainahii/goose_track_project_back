9f4c2c3763664c9b8ce8c31117d3b36f4081852a
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-08-30 12:52:49 +0300

    new reg1

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -4,7 +4,12 @@ require("dotenv").config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendEmail = async (date) => {
   const email = { ...date, from: "sanjaksms@gmail.com" };
-  await sgMail.send(email);
-  return true;
+  console.log("date :>> ", date);
+  try {
+    await sgMail.send(email);
+    return true;
+  } catch (error) {
+    return false;
+  }
 };
 module.exports = sendEmail;
