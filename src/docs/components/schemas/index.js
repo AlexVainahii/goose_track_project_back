c0d00935434280f9f7042597a6c20b82c91e8d89
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-09-03 02:01:50 +0300

    dok2

--- src/docs/components/schemas/index.js
+++ src/docs/components/schemas/index.js
@@ -1,12 +1,12 @@
 const User = require("./User");
 const Task = require("./Task");
 const Review = require("./Review");
-const updateUser = require("./updateUserTdo");
+const updateUserTdo = require("./updateUserTdo");
 module.exports = {
   schemas: {
     ...User,
     ...Task,
     ...Review,
-    ...updateUser,
+    ...updateUserTdo,
   },
 };
