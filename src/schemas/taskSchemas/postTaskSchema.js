07e0577d8ce68f28836a3490698faa48e49ed2f0
Andrew Ketsko <monicman82@gmail.com>
2023-09-03 18:59:13 +0300

    some

--- src/schemas/taskSchemas/postTaskSchema.js
+++ src/schemas/taskSchemas/postTaskSchema.js
@@ -1,6 +1,18 @@
 const Joi = require("joi");
 const { timeRegexp, dateRegexp } = require("@helpers");
 
+const validateStartEndTime = (obj, helpers) => {
+  function toMinute(time) {
+    const arrTime = time.split(":");
+    return Number(arrTime[0]) * 60 + Number(arrTime[1]);
+  }
+  const { start, end } = obj;
+
+  if (toMinute(start) >= toMinute(end)) {
+    return helpers.error("any.invalid");
+  }
+};
+
 const postTaskSchema = Joi.object({
   title: Joi.string()
     .required()
@@ -12,14 +24,19 @@ const postTaskSchema = Joi.object({
     .error(() => new Error("start")),
   end: Joi.string()
     .required()
-    .pattern(timeRegexp)
-    .error(() => new Error("end")),
+    .pattern(timeRegexp),
+    // .error(() => new Error("end")),
   priority: Joi.string().default("LOW").valid("LOW", "MEDIUM", "HIGH"),
   date: Joi.string()
     .required()
     .pattern(dateRegexp)
     .error(() => new Error("date")),
-  category: Joi.string().default("TODO").valid("TODO", "INPROGRESS", "DONE"),
-});
+  category: Joi.string()
+    .default("TODO")
+    .valid("TODO", "INPROGRESS", "DONE")
+}).custom(validateStartEndTime)
+    .messages({
+      "any.invalid": `The following condition must be met start<end`,
+    });
 
 module.exports = { postTaskSchema };
